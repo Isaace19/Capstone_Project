7d488fd4ddb77ba74821cd07c37b619a9945867a
Isaac <iabella.4576@gmail.com>
2024-10-12 19:39:36 -0400

    Changes to 2D node scenes, stil not working, but removal of camera and messing with collision physics might be the first cause to look into

---
+++ src/medival-game/movement.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+# set tile size in game, default to 16pixels
+# create a moving variable
+
+const tile_size = 16
+var moving = false
+
+var input_dir
+func _physics_process(delta: float) -> void:
+	input_dir = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		input_dir = Vector2(0,1)
+		move()
+	elif Input.is_action_pressed("ui_up"):
+		input_dir = Vector2(0,-1)
+		move()
+	elif Input.is_action_pressed("ui_right"):
+		input_dir = Vector2(1,0)
+		move()
+	elif Input.is_action_pressed("ui_left"):
+		input_dir = Vector2(-1,0)
+		move()
+	move_and_slide()
+
+func move():
+	# verify move when action is pressed
+	if input_dir:
+		if moving == false:
+			moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + input_dir*tile_size, 0.35)
+			tween.tween_callback(move_false) # moving is now false
+
+func move_false():
+	moving = false
